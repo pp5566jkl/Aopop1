6bc4498644c73a8a7ae1a90db3bbe986fbb469d1
恩 李 <4b1g0068@office.stust.edu.tw>
2024-09-24 14:35:26 +0800

    新增button2，picturebox2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             button1 = new Button();
+            pictureBox2 = new PictureBox();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -38,12 +41,13 @@
             pictureBox1.Location = new Point(60, 35);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(511, 359);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            button1.Location = new Point(296, 400);
+            button1.Location = new Point(274, 423);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
@@ -51,17 +55,39 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(656, 35);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(511, 359);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(879, 423);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "影像化灰階";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(640, 526);
+            ClientSize = new Size(1216, 526);
+            Controls.Add(button2);
+            Controls.Add(pictureBox2);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
@@ -69,5 +95,7 @@
 
         private PictureBox pictureBox1;
         private Button button1;
+        private PictureBox pictureBox2;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -14,15 +14,52 @@ namespace Aopop1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Image image;
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            try
             {
-                image = Image.FromFile(openFileDialog1.FileName);
-                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-                pictureBox1.Image = image;
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "圖像文件(JPeg, Gif, Bmp, etc.)|.jpg;*jpeg;*.gif;*.bmp;*.tif;*.tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap MyBitmap = new Bitmap(openFileDialog1.FileName);
+                    this.pictureBox1.Image = MyBitmap;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息顯示");
+            }
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < Width; x++)
+                    for (int y = 0; y < Height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        Result = (299 * r + 587 * g + 114 * b) / 1000;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                    }
+                this.pictureBox2.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息顯示");
             }
         }
+
+
     }
 }
