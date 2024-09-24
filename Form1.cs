721a8ed3be763d3a180f0f5ea77c1e72a09d6698
恩 李 <4b1g0068@office.stust.edu.tw>
2024-09-24 15:30:26 +0800

    7788

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Aopop1
 {
     public partial class Form1 : Form
@@ -7,12 +11,9 @@ namespace Aopop1
             InitializeComponent();
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
-
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -26,34 +27,44 @@ namespace Aopop1
                     this.pictureBox1.Image = MyBitmap;
                 }
             }
-
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "訊息顯示");
             }
-
-
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
             try
             {
+                // 設定亮度門檻
+                int threshold = 128; // 可以根據需要修改此值
+
                 int Height = this.pictureBox1.Image.Height;
                 int Width = this.pictureBox1.Image.Width;
                 Bitmap newBitmap = new Bitmap(Width, Height);
                 Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
                 Color pixel;
+
                 for (int x = 0; x < Width; x++)
+                {
                     for (int y = 0; y < Height; y++)
                     {
                         pixel = oldBitmap.GetPixel(x, y);
-                        int r, g, b, Result = 0;
-                        r = pixel.R;
-                        g = pixel.G;
-                        b = pixel.B;
-                        Result = (299 * r + 587 * g + 114 * b) / 1000;
-                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                        // 將 pixel 轉換為灰階
+                        int grayValue = (int)((299 * pixel.R + 587 * pixel.G + 114 * pixel.B) / 1000);
+
+                        // 根據門檻設定二值化像素值
+                        if (grayValue >= threshold)
+                        {
+                            newBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255)); // 設定為白色
+                        }
+                        else
+                        {
+                            newBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0)); // 設定為黑色
+                        }
                     }
+                }
                 this.pictureBox2.Image = newBitmap;
             }
             catch (Exception ex)
@@ -61,7 +72,5 @@ namespace Aopop1
                 MessageBox.Show(ex.Message, "訊息顯示");
             }
         }
-
-
     }
 }
